a7546f718eaf91bd77f86d11693369cabc0b816b
pepukrit <pepukrit@hotmail.com>
2024-05-01 17:31:14 +0700

    Added Counter.sol

---
+++ Counter.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+// Use 0.8.25+commit.b61c2a91 compiler on Remix VM
+pragma solidity ^0.8.0;
+
+contract Counter {
+    string private name;
+    uint private count;
+
+    constructor(string memory _name, uint _count) {
+        name = _name;
+        count = _count;
+    }
+
+    function increment() public returns (uint newCount) {
+        count++;
+        return count;
+    }
+
+    function decrement() public returns (uint newCount) {
+        count--;
+        return count;
+    }
+
+    function getCount() public view returns (uint newCount) {
+        return count;
+    }
+
+    function getName() public view returns (string memory newName) {
+        return name;
+    }
+
+    function setName(
+        string memory _name
+    ) public returns (string memory newName) {
+        name = _name;
+        return name;
+    }
+}
